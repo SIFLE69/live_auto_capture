283f5f33a5e4f31499e3099b2fb1006ddffa8414
SIFLE69 <siflebro@gmail.com>
2026-01-31 23:19:34 +0530

    fix: set relative base path for android assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4lth02ai1b"
+    "revision": "0.se8lclahoso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
